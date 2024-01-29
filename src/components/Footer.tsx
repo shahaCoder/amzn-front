a1f12cd0bfd6262b5e3a396ccb713d58aff07af3
Shoxrux Code <shoxruxpl04@gmail.com>
2024-01-29 11:10:57 -0500

    tik tok

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import { FaTiktok } from "react-icons/fa";
 export interface IAppProps {}
 
 export function Footer(props: IAppProps) {
@@ -20,13 +20,8 @@ export function Footer(props: IAppProps) {
                      </a>
                      <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                         <li>
-                           <a href="#" className="hover:underline me-4 md:me-6">
-                              Products
-                           </a>
-                        </li>
-                        <li>
-                           <a href="#" className="hover:underline">
-                              Contact
+                           <a href="https://www.tiktok.com/@amazonfinds.the.best?_t=8jRQW3nQ8XW&_r=1" className="hover:underline me-4 md:me-6">
+                              <FaTiktok size={'30px'}/>
                            </a>
                         </li>
                      </ul>
